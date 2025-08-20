da4924800141a2c20f374c261b6d1c67a103b4ba
Matías Hernández <matiashe@google.com>
2025-08-20 15:57:28 +0200

    Limit the number of services (NLSes, etc) that can be approved per user
    Trying to activate additional packages/components will be silently rejected.
    
    Bug: 428701593
    Test: atest ManagedServicesTest NotificationManagerServiceTest
    Flag: com.android.server.notification.limit_managed_services_count
    (cherry picked from commit a132684a093d9e1750100b39d4e4168f2d27d349)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:def30b58386ad8104aa6041952cacba473dc7ce2
    Merged-In: Iddd8044997c41f97369b768f4da5e49efc43ad06
    Change-Id: Iddd8044997c41f97369b768f4da5e49efc43ad06

--- services/tests/uiservicestests/src/com/android/server/notification/ManagedServicesTest.java
+++ services/tests/uiservicestests/src/com/android/server/notification/ManagedServicesTest.java
@@ -562,6 +562,23 @@ public class ManagedServicesTest extends UiServiceTestCase {
         assertFalse(service.isPackageOrComponentUserSet(componentName, 0));
     }
 
+    @Test
+    public void testPackageUninstall_componentNoLongerUserSetList() throws Exception {
+        final String pkg = "this.is.a.package.name";
+        final String component = pkg + "/Ba";
+        for (int approvalLevel : new int[] { APPROVAL_BY_COMPONENT, APPROVAL_BY_PACKAGE}) {
+            ManagedServices service = new TestManagedServices(getContext(), mLock, mUserProfiles,
+                    mIpm, approvalLevel);
+            writeExpectedValuesToSettings(approvalLevel);
+            service.migrateToXml();
+            final String verifyValue = (approvalLevel == APPROVAL_BY_COMPONENT) ? component : pkg;
+            assertThat(service.isPackageOrComponentAllowed(verifyValue, 0)).isTrue();
+            assertThat(service.isPackageOrComponentUserSet(verifyValue, 0)).isTrue();
+            service.onPackagesChanged(true, new String[]{pkg}, new int[]{103});
+            assertThat(service.isPackageOrComponentUserSet(verifyValue, 0)).isFalse();
+        }
+    }
+
     /** Test that backup only writes packages/components that belong to the target user. */
     @Test
     public void testWriteXml_onlyBackupsForTargetUser() throws Exception {
