b1ffe5b9bbc00f6981ecb6a2be9ebae3bec15e53
Pavel Grafov <pgrafov@google.com>
2025-12-02 17:12:41 +0000

    [BACKPORT] Don't allow hiding critical apps.
    Hiding these apps in the work profile may cause failure to boot.
    
    Bug: 438186009
    Test: atest CtsDevicePolicyManagerTestCases:com.android.cts.devicepolicy.ManagedProfileTest#testHideAllApps
    Flag: EXEMPT BUGFIX
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:79cca47a6201c9debdf5695b240926c3f1944166
    Merged-In: I2ade10f20636fe8dfe875f4827e5e97d734bf8a1
    Change-Id: I2ade10f20636fe8dfe875f4827e5e97d734bf8a1

--- services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
+++ services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -9884,6 +9884,8 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
                 R.array.config_packagesExemptFromSuspension);
         dumpResources(pw, mContext, "policy_exempt_apps", R.array.policy_exempt_apps);
         dumpResources(pw, mContext, "vendor_policy_exempt_apps", R.array.vendor_policy_exempt_apps);
+        dumpResources(pw, mContext, "application_hidden_policy_exempt_apps",
+                R.array.application_hidden_policy_exempt_apps);
         pw.decreaseIndent();
         pw.println();
     }
@@ -11553,7 +11555,7 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
                 || (caller.hasPackage() && isCallerDelegate(caller, DELEGATION_PACKAGE_ACCESS)));
 
         List<String> exemptApps = listPolicyExemptAppsUnchecked();
-        if (exemptApps.contains(packageName)) {
+        if (exemptApps.contains(packageName) || isPackageExemptFromHiding(packageName)) {
             Slogf.d(LOG_TAG, "setApplicationHidden(): ignoring %s as it's on policy-exempt list",
                     packageName);
             return false;
@@ -11855,6 +11857,17 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
             }
             return resultSet.toArray(new String[resultSet.size()]);
         }
+    }
+
+    private boolean isPackageExemptFromHiding(@NonNull String packageName) {
+        String[] exemptPackages = mContext.getResources().getStringArray(
+                R.array.application_hidden_policy_exempt_apps);
+        for (String exemptPackage : exemptPackages) {
+            if (packageName.equals(exemptPackage)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
