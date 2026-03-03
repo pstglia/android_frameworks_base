129a3c502afdd2018e99298e211e00279057fa9d
Sanjana Sunil <sanjanasunil@google.com>
2026-03-03 17:27:54 +0000

    Compare period character as well during wildcard matching
    Currently, during wildcard matching, specifying *.xyz.com will end up
    matching any domain ending in xyz.com (e.g. including abcxyz.com, which
    is completely different). Ensure that it only matches the following:
    1. xyz.com exactly
    2. Domains ending in .xyz.com i.e. check the period character as well
    
    Bug: 483142784
    Flag: EXEMPT BUGFIX
    Test: atest DomainVerificationPackageTest#testWildcardDomain
    
    Change-Id: Ib6c7d9b560c75dd8ce1be3e2e40c68f8f3238f14
    Merged-In: Ib6c7d9b560c75dd8ce1be3e2e40c68f8f3238f14
    (cherry picked from commit 919d0cb125b31e1512417a69251f1cbee54ab12e)

--- services/tests/PackageManagerServiceTests/unit/src/com/android/server/pm/test/verify/domain/DomainVerificationPackageTest.kt
+++ services/tests/PackageManagerServiceTests/unit/src/com/android/server/pm/test/verify/domain/DomainVerificationPackageTest.kt
@@ -78,6 +78,31 @@ class DomainVerificationPackageTest {
     private val pkg2 = mockPkgSetting(PKG_TWO, UUID_TWO, SIGNATURE_TWO)
 
     @Test
+    fun testWildcardDomain() {
+        val wildcardDomain = "*.xyz.com"
+        val pkg = mockPkgState(
+            PKG_ONE, UUID_ONE, SIGNATURE_ONE,
+            autoVerifyDomains = listOf(wildcardDomain)
+        )
+
+        val service = makeService(pkg)
+        service.addPackage(pkg)
+        service.setStatus(pkg.domainSetId, setOf(wildcardDomain), STATE_SUCCESS)
+
+        // Only "xyz.com" exactly or subdomains should match. "abcxyz.com" should not match.
+        assertThat(
+            service.getOwnersForDomain("abcxyz.com", USER_ID))
+            .isEmpty()
+        assertThat(
+            service.getOwnersForDomain("abc.xyz.com", USER_ID))
+            .containsExactly(DomainOwner(PKG_ONE, false))
+        assertThat(
+            service.getOwnersForDomain("xyz.com", USER_ID))
+            .containsExactly(DomainOwner(PKG_ONE, false))
+    }
+
+
+    @Test
     fun addPackageFirstTime() {
         val service = makeService(pkg1, pkg2)
         service.addPackage(pkg1)
