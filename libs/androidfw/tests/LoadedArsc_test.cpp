82ecad6d538824fce3891b2267070c6c38f7ce25
Yurii Zubrytskyi <zyy@google.com>
2026-03-23 14:50:08 -0700

    Validate package ID range in LoadedArsc
    The package ID in a RES_TABLE_PACKAGE_TYPE chunk is 32-bit,
    but resource IDs only allocate 8 bits for the package ID.
    If a malformed ARSC file has a package ID greater than 255, it
    could lead to unexpected behavior or overflow when being parsed.
    
    This change adds a range check to ensure the package ID is within the
    valid range during loading.
    
    Test: atest libandroidfw_tests
    Bug: 484973621
    Flag: EXEMPT CVE_FIX
    Change-Id: I438cd0e7f64e2b0ef4cc2bcb4f1f81778216e5e8

--- libs/androidfw/tests/LoadedArsc_test.cpp
+++ libs/androidfw/tests/LoadedArsc_test.cpp
@@ -399,6 +399,30 @@ TEST(LoadedArscTest, LoadCustomLoader) {
   ASSERT_TRUE(LoadedPackage::GetEntry(type.type, entry_index).has_value());
 }
 
+TEST(LoadedArscTest, LoadArscWithInvalidPackageId) {
+  struct {
+    ResTable_header table_header;
+    ResTable_package package_header;
+  } __attribute__((packed)) data = {};
+
+  data.table_header.header.type = htods(RES_TABLE_TYPE);
+  data.table_header.header.headerSize = htods(sizeof(data.table_header));
+  data.table_header.header.size = htodl(sizeof(data));
+  data.table_header.packageCount = htodl(1);
+
+  data.package_header.header.type = htods(RES_TABLE_PACKAGE_TYPE);
+  data.package_header.header.headerSize = htods(sizeof(data.package_header));
+  data.package_header.header.size = htodl(sizeof(data.package_header));
+  data.package_header.id = htodl(256); // Invalid ID (> 255)
+  data.package_header.typeStrings = htodl(sizeof(data.package_header));
+  data.package_header.keyStrings = htodl(sizeof(data.package_header));
+
+  auto loaded_arsc = LoadedArsc::Load(&data, sizeof(data));
+  ASSERT_THAT(loaded_arsc, IsNull())
+      << "Expected to fail loading but got " << loaded_arsc->GetPackages().size()
+      << " packages, first ID " << loaded_arsc->GetPackages().at(0)->GetPackageId();
+}
+
 // structs with size fields (like Res_value, ResTable_entry) should be
 // backwards and forwards compatible (aka checking the size field against
 // sizeof(Res_value) might not be backwards compatible.
